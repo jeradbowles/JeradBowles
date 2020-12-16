013533cb265519cd8c4315cc1b5fc419335babd8
Noshin Rahman <nr0869031@student.francistuttle.edu>
2020-12-15 18:39:32 -0600

    fix javascript validation and media queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 const submitBtn = document.getElementById('myButton');
 
 const validate = (e) => {
-    e.preventDefault();
-    const name = document.getElementById('firstname');
-    if (firstname.value === "") {
-        alert("Please enter your first name.");
-        firstname.focus();
-        return false;
-    }
-    
-    return true;
+  e.preventDefault();
+  const firstname = document.getElementById('firstname');
+  const emailAddress = document.getElementById('email-address');
+  if (firstname.value === "") {
+    alert("firstname enter your first name.");
+    firstname.focus();
+    return false;
+  }
+  if (lastename.value === "") {
+    alert("firstname enter your last name.");
+    lastname.focus();
+    return false;
+  }
+  if (email.value === "") {
+    alert("Please enter your email address.");
+    email.focus();
+    return false;
+  }
+  
+  return true;
 }
 
 submitBtn.addEventListener('click', validate);
